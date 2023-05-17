c4a65cee7ffb20aef16a5c566897a930e745f12c
Rashed967 <rashedhasan967@gmail.com>
2023-05-17 09:53:46 +0600

    added paginatoin

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,14 +3,25 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
 
+    const {totalProducts} = useLoaderData()
+    const itemsPerPage = 10
+    const totalPage = Math.ceil(totalProducts / itemsPerPage)
+    const pageNumbers = [...Array(totalPage).keys()]
+
+    console.log(totalProducts)
+
+
+
+
+
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
@@ -21,7 +32,7 @@ const Shop = () => {
         // step 1: get id of the addedProduct
         for (const id in storedCart) {
             // step 2: get product from products state by using id
-            const addedProduct = products.find(product => product.id === id)
+            const addedProduct = products.find(product => product._id === id)
             if (addedProduct) {
                 // step 3: add quantity
                 const quantity = storedCart[id];
@@ -41,19 +52,19 @@ const Shop = () => {
         // const newCart = [...cart, product];
         // if product doesn't exist in the cart, then set quantity = 1
         // if exist update quantity by 1
-        const exists = cart.find(pd => pd.id === product.id);
+        const exists = cart.find(pd => pd._id === product._id);
         if (!exists) {
             product.quantity = 1;
             newCart = [...cart, product]
         }
         else {
             exists.quantity = exists.quantity + 1;
-            const remaining = cart.filter(pd => pd.id !== product.id);
+            const remaining = cart.filter(pd => pd._id !== product._id);
             newCart = [...remaining, exists];
         }
 
         setCart(newCart);
-        addToDb(product.id)
+        addToDb(product._id)
     }
 
     const handleClearCart = () => {
@@ -62,11 +73,12 @@ const Shop = () => {
     }
 
     return (
+        <>
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
@@ -83,6 +95,13 @@ const Shop = () => {
                 </Cart>
             </div>
         </div>
+        {/* pagination  */}
+        <div className="pagination">
+            {
+               pageNumbers.map(pageNumber => <button key={pageNumber}>{pageNumber}</button>) 
+            }
+        </div>
+        </>
     );
 };
 
